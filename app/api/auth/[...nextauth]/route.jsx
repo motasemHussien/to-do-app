22ed0d4919e277318b821da14d90c6bb53e7f674
Motasem Hussein <motasem.hussien97@gmail.com>
2023-07-01 23:21:49 +0300

    create register page

--- app/api/auth/[...nextauth]/route.jsx
+++ app/api/auth/[...nextauth]/route.jsx
@@ -1,41 +1,48 @@
 import NextAuth from "next-auth/next";
 import prisma from "../../../libs/prismadb";
 import { PrismaAdapter } from "@next-auth/prisma-adapter";
-import CredentialsProvider  from "next-auth/providers/credentials";
-import GoogleProvider from "next-auth/providers/google"
-import GithubProvider from "next-auth/providers/github"
+import CredentialsProvider from "next-auth/providers/credentials";
+import GoogleProvider from "next-auth/providers/google";
+import GithubProvider from "next-auth/providers/github";
 
+export const authOptions = {
+  adapter: PrismaAdapter(prisma),
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_ID,
+      clientSecret: process.env.GOOGLE_SECRET,
+    }),
+    GithubProvider({
+      clientId: process.env.GITHUB_ID,
+      clientSecret: process.env.GITHUB_SECRET,
+    }),
+    CredentialsProvider({
+      name: "credentials",
+      credentials: {
+        email: { label: "Email", type: "text", placeholder: "jsmith" },
+        password: { label: "Password", type: "password" },
+        username: {
+          label: "Username",
+          type: "text",
+          placeholder: "Ahmad Mo7sen",
+        },
+      },
+      async authorize() {
+        const user = {
+          id: 1,
+          name: "Ahmad Mo7sen",
+          email: "beberebo@Ahmadmo7sen.com",
+        };
+        return user;
+      },
+    }),
+  ],
+  secret: process.env.SECRET,
+  session: {
+    strategy: "jwt",
+  },
+  debug: process.env.NODE_ENV === "development",
+};
 
-export const authOptions= {
-    adapter: PrismaAdapter(prisma),
-    provider:[
-        GithubProvider({
-            clientId : process.env.GITHUB_ID,
-            clientSecret: process.env.GITHUB_SECRET,
-        }),
-        GoogleProvider({
-            clientId : process.env.GOOGLE_ID,
-            clientSecret: process.env.GOOGLE_SECRET,
-        }),
-        CredentialsProvider({
-            name: "Credentials",
-            credentials: {
-                email:{label:"Email",type:"text",placeholder:"mhussein"},
-                password:{label:"Password", type:"password"},
-                username:{label: "Username",type:"text",placeholder:"Motasem Hussein"},
-            },
-            async authorize(Credentials){
-                const user = {id:1, name:"motasem", email:"mota@gmail.com"} //this is fake data
-                return user;
-            },
-        }),
-    ],
-    secret: process.env.SECRET,
-    session:{
-        strargy:"jwt",
-    },
-    debug: process.env.NODE_ENV === 'development',
-
-}
-const handler = NextAuth(authOptions)
-export {handler as GET , handler as POST}
+const handler = NextAuth(authOptions);
+export { handler as GET, handler as POST };
--- app/page.js
+++ app/page.js
@@ -1,7 +1,16 @@
-export default function Home() {
+import { getServerSession } from "next-auth";
+import { authOptions } from "./api/auth/[...nextauth]/route";
+import User from "./components/user";
+
+export default async function Home() {
+  const session = await getServerSession(authOptions);
+
   return (
     <section>
-      <h1>Home</h1>
+      <div>Home</div>
+      <pre>{JSON.stringify(session)}</pre>
+      <h1>client side</h1>
+      <User />
     </section>
   );
 }
